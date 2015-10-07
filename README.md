9b44819e6a1d64aafd41f0b814dab9938da9a2aa
Brittany Welsh <hello+world@brittanywelsh.com>
2015-10-07 21:19:42 +0000

    Adds initial installation scripts via setup.sh, plus customized 'cheat sheet' shell script.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # django-dev-env
-Simple command-line scripts for setting up an Ubuntu system for Django development.
+## Quick development-environment setup for lazy people.
+
+This is a simple bash script that runs a list of installation commands, to simplify setup of a new development VM. It is intended to be used on a fresh Ubuntu installation.
+
+### What gets installed?
+
+Here's the full breakdown of what the `setup.sh` script runs:
+1. Update/upgrade apt-get packages
+2. Install python-setuptools (via apt-get)
+3. Install pip (via easy-install)
+4. Install virtualenv (via pip, system-wide)
+5. Install pyenv
+6. Install Python 2.7 and Python 3 (via pyenv)
+8. Install "cheat sheet" (included in this repo)
+
+### Usage
+
+From your development machine, first download the repo. Next, edit the setup script as you please, then run it from within the `django-dev-env` directory with `./setup.sh`. The full list of commands is:
+
+```
+git clone https://github.com/brittanywelsh/django-dev-env.git
+cd django-dev-env
+chmod u+x setup.sh
+./setup.sh
+```
+
+NOTE: if you want to install the cheat sheet, make sure to run the script from within the `django-env-dev` directory. After the script finishes running, you won't need the repo any more so you can happily delete it.
+
+You can also keep the `django-dev-env` folder around if you want to make your own modifications to the cheat sheet. To re-install a modified cheat sheet, simply run the `install-cheatsheet.sh` script from within the `django-dev-env` folder.
---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Variables - for setting up Git profile
+GIT_NAME="Brittany Welsh"
+GIT_EMAIL="hello+world@brittanywelsh.com"
+GIT_EDITOR="vim"
+
+# Variables - change these to customize your setup!
+PYTHON2_VERSION="2.7.10"
+PYTHON3_VERSION="3.5.0"
+RUBY_VERSION="2.2.3"
+LOG_FILE_PATH="$HOME/.my_dev-env_setup.txt"
+CHEAT_SHEET_PATH="/usr/local/bin/"
+CHEAT_SHEET_NAME="cheat"
+
+###
+echo "Starting the DjangonDevEnv setup process... sit tight!"
+###
+
+{
+  # Updating Ubuntu packages
+  echo -e "\n===\nUpdating Ubuntu Packages\n==="
+  sudo apt-get update
+  sudo apt-get upgrade
+
+  # Install preliminary python packages
+  echo -e "\n===\nInstalling Python Packages\n==="
+  sudo apt-get install python-setuptools
+  sudo easy_install pip
+  sudo pip install -U pip
+  sudo pip install virtualenv
+
+  # Install pyenv
+  echo -e "\n===\nInstalling pyenv\n==="
+  curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+  export PATH="$HOME/.pyenv/bin:$PATH"
+
+  # Use pyenv to install python
+  echo -e "\n===\nUse pyenv to install Python 2 & 3\n==="
+  pyenv update
+  pyenv install $PYTHON2_VERSION
+  pyenv install $PYTHON3_VERSION
+  pyenv global $PYTHON3_VERSION
+
+  # Configure git
+  git config --global user.name "Brittany Welsh"
+  git config --global user.email "hello+world@brittanywelsh.com"
+  git config --global core.editor "vim"
+
+  # Install the cheat sheet and add it to $PATH
+  echo -e "\n===\nAdding Cheat Sheet script\n==="
+  sudo cp cheat $CHEAT_SHEET_PATH
+  sudo chmod +x $CHEAT_SHEET_PATH/$CHEAT_SHEET_NAME
+  sudo chmod +x install-cheatsheet.sh
+
+  echo -e "\n=== End of installation script! ==="
+
+} 2>&1 | tee $LOG_FILE_PATH
+
+echo -e "\nAll done!\nThe printout above has also been saved to this file: $LOG_FILE_PATH \n"
